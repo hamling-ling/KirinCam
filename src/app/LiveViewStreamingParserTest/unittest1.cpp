39de858d57b12290298127f9bb120943359e925f
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2015-11-17 01:28:47 +0900

    moved stream parse test code to unit test

--- src/app/LiveViewStreamingParserTest/unittest1.cpp
+++ src/app/LiveViewStreamingParserTest/unittest1.cpp
@@ -1,7 +1,13 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "LiveViewStreamParser.h"
+#include <fstream>
+#include <iostream>
+#include <boost/asio.hpp>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+using namespace boost;
 
 namespace LiveViewStreamingParserTest
 {		
@@ -9,10 +15,26 @@ namespace LiveViewStreamingParserTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestDataFileExist)
 		{
-			// TODO: テスト コードをここに挿入します
+			asio::streambuf buf;
+			ifstream ifs("out.dat", ios::in | ios::binary);
+			if (!ifs) {
+				Assert::Fail(L"failed to load out.dat");
+			}
+			
 		}
 
+		TEST_METHOD(TestParsePacket) {
+
+			asio::streambuf buf;
+			ifstream ifs("out.dat", ios::in | ios::binary);
+
+			LiveViewPacket packet0;
+			Assert::IsTrue(packet0.Fill(buf));
+
+			LiveViewPacket packet1;
+			Assert::IsTrue(packet1.Fill(buf));
+		}
 	};
 }
