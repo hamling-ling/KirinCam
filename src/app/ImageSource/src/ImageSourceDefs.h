23a50c2f8719e37776983dd8d2e3b87e139cea2d
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2015-12-17 02:55:37 +0900

    add CameraFrameData definitions

---
+++ src/app/ImageSource/src/ImageSourceDefs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+struct FramePoint {
+	uint16_t x;
+	uint16_t y;
+};
+
+struct CamFrameRect {
+	FramePoint bottomRight;
+	FramePoint topLeft;
+};
+
+struct CameraFrameInformation {
+	CamFrameRect rect;
+	uint8_t category;
+	uint8_t status;
+	uint8_t status2;
+};
+
+struct CameraFrame {
+	uint16_t sequenceNumber;
+	uint32_t timeStamp;
+	std::vector<uint8_t> image;
+	std::vector<CameraFrameInformation> info;
+};
