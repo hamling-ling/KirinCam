c6fa03aed433c89b34222031600015ee4ad4d003
hamling-ling <tacobell@gj9.so-net.ne.jp>
2016-01-20 01:26:50 +0900

    update opencv version

--- src/app/ImageSource/src/opencvlib.h
+++ src/app/ImageSource/src/opencvlib.h
@@ -1,14 +1,12 @@
 
-//プロジェクトのプロパティ⇒C/C++⇒全般　の追加のインクルードディレクトリに
-// 『C:\OpenCV2.2\include』を追加のこと
 #include <opencv2/opencv.hpp>
 
 #ifdef _DEBUG        
-#pragma comment(lib,"opencv_core300d.lib")
-#pragma comment(lib,"opencv_imgproc300d.lib")
-#pragma comment(lib,"opencv_highgui300d.lib")
-//#pragma comment(lib,"opencv_hal300d.lib")
-#pragma comment(lib,"opencv_imgcodecs300d.lib")
+#pragma comment(lib,"opencv_core310d.lib")
+#pragma comment(lib,"opencv_imgproc310d.lib")
+#pragma comment(lib,"opencv_highgui310d.lib")
+//#pragma comment(lib,"opencv_hal310d.lib")
+#pragma comment(lib,"opencv_imgcodecs310d.lib")
 //#pragma comment(lib,"ippicvmt.lib")
 //#pragma comment(lib,"IlmImfd.lib")
 //#pragma comment(lib,"libjasperd.lib")
@@ -18,11 +16,11 @@
 //#pragma comment(lib,"libwebpd.lib")
 //#pragma comment(lib,"zlibd.lib")
 #else    
-#pragma comment(lib,"opencv_core300.lib")
-#pragma comment(lib,"opencv_imgproc300.lib")
-#pragma comment(lib,"opencv_highgui300.lib")
-#pragma comment(lib,"opencv_hal300.lib")
-#pragma comment(lib,"opencv_imgcodecs300.lib")
+#pragma comment(lib,"opencv_core310.lib")
+#pragma comment(lib,"opencv_imgproc310.lib")
+#pragma comment(lib,"opencv_highgui310.lib")
+#pragma comment(lib,"opencv_hal310.lib")
+#pragma comment(lib,"opencv_imgcodecs310.lib")
 #pragma comment(lib,"ippicvmt.lib")
 #pragma comment(lib,"IlmImf.lib")
 #pragma comment(lib,"libjasper.lib")
